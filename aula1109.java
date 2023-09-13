f9028cfbd188195ce915bd164766d498de328cd1
Renata Marinho <renatareismarinho@gmail.com>
2023-09-12 21:38:06 -0300

    Update aula1109.java

--- aula1109.java
+++ aula1109.java
@@ -1,57 +1,74 @@
+package aplication;
+
+import java.util.Locale;
 import java.util.Scanner;
 
-public class MyClass {
-    public static void main(String args[]) {
-      Scanner sc = new Scanner(System.in);
-      
-      int numUm, numDois, numTres, numQuatro, numCinco, raio;
-      int operation;
-      double pi = 3.14;
-      
-      System.out.println("Digite a operação desejada: ");
-      operation = sc.nextInt();
-      sc.nextLine();
-      
-      switch(operation){
-          
-          case 1:
-              System.out.println("Digite dois numeros a serem divididos: ");
-              numUm = sc.nextInt();
-              numDois = sc.nextInt();
-              
-              int divisao = numUm / numDois;
-              
-              System.out.println("A diferenca e: " + divisao);
-              break;
-              
-           case 2:  
-                System.out.println("Digite o valor do raio: ");
-                raio = sc.nextInt();
-                
-                double area = pi * (raio * raio);
-                System.out.println("A area do circulo e: " + area);
-                break;
-        
-          case 3:
-              System.out.println("Digite os cinco numeros: ");
-              numUm = sc.nextInt();
-              numDois = sc.nextInt();
-              numTres = sc.nextInt();
-              numQuatro = sc.nextInt();
-              numCinco = sc.nextInt();
-              
-              double media = (numUm + numDois + numTres + numQuatro + numCinco)/5;
-              
-              System.out.println("A media dos numeros e: " + media);
-              break;
-        case 4:
-            System.out.print("FIM DO PROGRAMA!!!");
-              break;
-         default:
-              System.out.print("VALOR INVALIDO CARALHO!!!");
-              break;
-      }
-      
-      sc.close();
-    }
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double num1, num2, raio;
+		int operacao = 0;
+		 
+	
+	while(operacao != 4){
+		
+		System.out.print("Digite a operação desejada: ");
+		operacao = sc.nextInt();
+		sc.nextLine(); 
+		
+		switch(operacao) {
+		 case 1:
+			 System.out.println("Digite dois numeros a serem divididos: ");
+			 num1 = sc.nextInt();
+             num2 = sc.nextInt();
+             double divisao = num1 / num2 ;
+            
+            System.out.printf("A diferenca é: %.2f%n" , divisao);
+            break;
+		 case 2:
+			 System.out.print("Digite o valor do raio: ");
+            raio = sc.nextInt();
+            sc.nextLine();
+            
+            double area = Math.PI * Math.pow(raio,2);
+            
+            System.out.printf("A area do circulo é: %.2f%n", area);
+            break;
+		 case 3:
+			 System.out.println("Digite os numeros a serem calculados: ");
+			 
+			 double[] numeros = new double[5];
+			 
+			 for (int i=0; i<5; i++) {
+				 numeros[i] = sc.nextDouble();
+			 }
+			 
+			 double soma = 0.0;
+			 for (int i=0; i<5; i++) {
+				 soma += numeros[i];
+			 }
+			 
+			 double media = soma / 5;
+			 
+			 System.out.printf("A média arititédica é: %.2f%n", media);
+			 
+			 break;
+		 case 4:
+	            System.out.print("FIM DO PROGRAMA!!!");
+	              break;
+	     default:
+	              System.out.println("VALOR INVALIDO CARALHO!!!");
+	              break;
+		}
+		
+	}
+			
+		sc.close();
+	}
+
 }
+
